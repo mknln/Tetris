bb9e39a420964cf1328eb0737788a8d730e6763a
mpnolan <mpnolan@gmail.com>
2012-03-03 14:07:01 -0500

    Update tetris/NextPieceCanvas.java

--- tetris/NextPieceCanvas.java
+++ tetris/NextPieceCanvas.java
@@ -1,6 +1,9 @@
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
+ *
+ * ------------------
+ * Testing GitHub's edit feature.
  */
 
 package tetris;
