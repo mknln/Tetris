03980f57a022fee9612f3459cc69220a8834f6e2
Mike Nolan <mpnolan@gmail.com>
2012-03-05 19:23:38 -0500

    testing

--- tetris/Main.java
+++ tetris/Main.java
@@ -20,6 +20,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Create game.
         TetrisGame game = new TetrisGame();
         DisplayFrame frame = new DisplayFrame(game);
     }
